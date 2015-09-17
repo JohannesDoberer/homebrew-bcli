f9f8da088a8d60167f7a5b0283489b1da2d214b1
JohannesDoberer <johannes.doberer@sap.com>
2015-09-17 15:57:46 +0200

    no message

---
+++ bcli.rb
@@ -0,0 +1,21 @@
+require 'formula'
+
+class Bcli < Formula
+  homepage 'https://github.com/hybris/builder-cli/tree/feature/homebrew/dist/homebrew-builder'
+  head 'https://github.com/hybris/builder-cli/blob/feature/homebrew/dist/homebrew-builder/builder.tgz?raw=true'
+  url  'https://github.com/hybris/builder-cli/blob/feature/homebrew/dist/homebrew-builder/builder.tgz?raw=true'
+  version '0.0.1'
+  sha1 'afd2073e34c1b092e298905a98ceee21166e3540'
+
+  depends_on :arch => :x86_64
+  
+  def install
+      bin.install 'builder'
+  end
+
+  test do
+    system "#{bin}/builder"
+  end
+end
+
+    
