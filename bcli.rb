717427e62a5f9a38ee9c89decdc820ba03c2cbfb
JohannesDoberer <johannes.doberer@sap.com>
2015-09-18 12:27:01 +0200

    no message

--- bcli.rb
+++ bcli.rb
@@ -2,7 +2,6 @@ require 'formula'
 
 class Bcli < Formula
   homepage 'https://github.com/SAP/builder-cli/tree/feature/homebrew/dist/homebrew-builder'
-  head 'https://github.com/SAP/builder-cli/blob/feature/homebrew/dist/homebrew-builder/builder.tgz?raw=true'
   url  'https://github.com/SAP/builder-cli/blob/feature/homebrew/dist/homebrew-builder/builder.tgz?raw=true'
   version '0.0.1'
   sha1 '3ece0cca420609e24a7afc9cee54aa895c2caefd'
