5fb16d9b28ccd23db417e1a64e9b7645e4d49372
JohannesDoberer <johannes.doberer@sap.com>
2015-09-18 12:24:19 +0200

    no message

--- bcli.rb
+++ bcli.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class Bcli < Formula
-  homepage 'https://github.com/hybris/builder-cli/tree/feature/homebrew/dist/homebrew-builder'
-  head 'https://github.com/hybris/builder-cli/blob/feature/homebrew/dist/homebrew-builder/builder.tgz?raw=true'
-  url  'https://github.com/hybris/builder-cli/blob/feature/homebrew/dist/homebrew-builder/builder.tgz?raw=true'
+  homepage 'https://github.com/SAP/builder-cli/tree/feature/homebrew/dist/homebrew-builder'
+  head 'https://github.com/SAP/builder-cli/blob/feature/homebrew/dist/homebrew-builder/builder.tgz?raw=true'
+  url  'https://github.com/SAP/builder-cli/blob/feature/homebrew/dist/homebrew-builder/builder.tgz?raw=true'
   version '0.0.1'
-  sha1 '73828faf161abec3e81be7d7e98e2f9af59969f8'
+  sha1 '3ece0cca420609e24a7afc9cee54aa895c2caefd'
 
   depends_on :arch => :x86_64
   
